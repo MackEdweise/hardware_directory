0c1abfcc7991e3f5cdd046c304860e2e17c1de32
Marcus Edwards <marcusedwards@hotmail.ca>
2017-11-02 23:11:00 -0400

    add SSH facade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
         @endforeach
     </div>
 </div>
-<div class="container space">
+<div class="space">
     <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
         <div class="panel panel-default">
             <div class="list-group">
